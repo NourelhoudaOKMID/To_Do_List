9ad060cbec3cf384cacc6c7f14bde5df9e793b3e
NourelhoudaOKMID <nour.okmid@gmail.com>
2026-01-08 03:57:59 +0100

    feat:  search the element that we need

---
+++ public/Js/app.js
@@ -0,0 +1,6 @@
+const input = document.getElementById("taskInput");
+const addBtn = document.getElementById("addBtn");
+const container = document.getElementById("tasksContainer");
+const template = document.getElementById("taskTemplate");
+const feedback = document.getElementById("feedback");
+
