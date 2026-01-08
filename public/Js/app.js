830362b2bbc507701ee5258d899e2f43042fd5c0
NourelhoudaOKMID <nour.okmid@gmail.com>
2026-01-08 04:03:10 +0100

    feat:  clone the tamplate html with the js

--- public/Js/app.js
+++ public/Js/app.js
@@ -4,3 +4,38 @@ const container = document.getElementById("tasksContainer");
 const template = document.getElementById("taskTemplate");
 const feedback = document.getElementById("feedback");
 
+
+function renderTasks() {
+  container.innerHTML = "";
+
+  tasks.forEach((task, index) => {
+    const clone = template.content.cloneNode(true);
+
+    const taskDiv = clone.querySelector(".task");
+    const textSpan = clone.querySelector(".task-text");
+    const checkBtn = clone.querySelector(".check-btn");
+    const editBtn = clone.querySelector(".edit-btn");
+    const deleteBtn = clone.querySelector(".delete-btn");
+
+    textSpan.textContent = task.text;
+
+    if (task.completed) {
+      taskDiv.classList.add("completed");
+    } else {
+      taskDiv.classList.remove("completed");
+    }
+
+
+
+
+
+
+
+
+    container.appendChild(clone);
+  });
+
+}
+
+
+renderTasks();
